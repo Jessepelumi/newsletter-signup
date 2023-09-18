03f24ace115aacfcb5382ea400185da8bd1b8755
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-09-18 16:41:21 +0100

    almost done with the structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      listStyleImage: {
+        checkmark: 'url("assets/images/icon-list.svg")'
+      }
+    },
   },
   plugins: [],
 }
