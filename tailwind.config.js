e4649e6a9f3e2a493e3486b0300b4b27ebcdea76
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-09-19 15:48:15 +0100

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     extend: {
       listStyleImage: {
         checkmark: 'url("assets/images/icon-list.svg")'
+      },
+      colors: {
+        primary: {
+          "tomato": "hsl(4, 100%, 67%)"
+        },
+        neutral: {
+          "darkSlate": "hsl(234, 29%, 20%)",
+          "charcoalGrey": "hsl(235, 18%, 26%)",
+          "grey": "hsl(231, 7%, 60%)",
+          "white": "hsl(0, 0%, 100%)"
+        }
       }
     },
   },
