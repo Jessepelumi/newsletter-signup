89fabc90e1538d562581b3d24ebf1c7f2bb557b8
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-09-20 23:13:14 +0100

    added custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
           "grey": "hsl(231, 7%, 60%)",
           "white": "hsl(0, 0%, 100%)"
         }
+      },
+      fontFamily: {
+        custom: ["Roboto", "sans-serif"]
       }
     },
   },
