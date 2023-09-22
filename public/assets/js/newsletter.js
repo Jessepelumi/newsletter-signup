bcd087dd9fbe9fcd84cbdc84c60ea7e687e7d161
Adesina Jesse <jesseoluwapelumi01@gmail.com>
2023-09-22 20:22:15 +0100

    almost done with email validation

--- public/assets/js/newsletter.js
+++ public/assets/js/newsletter.js
@@ -1,6 +1,35 @@
+const form = document.getElementById("form");
+
 const emailExpression = /^([a-z0-9\.-]+)@([a-z0-9-]+)\.([a-z]{2,8})([a-z]{2,8})?$/;
 const emailInput = document.getElementById("email");
+const errorMessage = document.getElementById("error-message");
 
 function isEmailValid(email) {
     return emailExpression.test(email);
 }
+
+form.addEventListener("submit", (e) => {
+    const email = emailInput.value;
+    console.log(email);
+    console.log(isEmailValid(email));
+
+    emailInput.addEventListener("input", () => {
+        if (emailInput.classList.contains("error")) {
+            emailInput.classList.remove("error");
+            errorMessage.classList.add("hidden");
+        }
+    });
+
+    if (email == "") {
+        emailInput.classList.add("error");
+        errorMessage.classList.remove("hidden");
+    }
+
+    if(!isEmailValid(email)) {
+        emailInput.classList.add("error");
+        errorMessage.classList.remove("hidden");
+    }
+
+    
+    e.preventDefault();
+});
